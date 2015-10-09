664ecacb69435ff06f9a4833c42b52441c47ca27
SanyaTv <aleks.tv30@gmail.com>
2015-10-09 21:55:09 +0300

    HW1
    New group creation cases.

---
+++ addressbook-selenium-tests/src/com/example/tests/EmptyGroupCreationTests.java
@@ -0,0 +1,16 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class EmptyGroupCreationTests extends TestBase {
+  
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	openMainPage();
+    goToGroupsPage();
+    initGroupsCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
